d295313a95917f1ff511a01561af26a8782ca52f
PotassiumFl <3390945494@qq.com>
2026-03-17 10:32:05 +0800

    feat: create hello controller

---
+++ src/test/java/org/example/sedemo/SeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
